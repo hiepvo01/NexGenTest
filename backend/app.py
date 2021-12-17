3f3681bfb0757da0f66a3545fd80393cf3fad513
hiepvo01 <vohi01@luther.edu>
2021-12-16 20:44:16 -0600

    model route

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
 from flask_marshmallow import Marshmallow
+import pickle
 import os
+import numpy as np
 
 app = Flask(__name__)
 
@@ -24,6 +26,27 @@ def all_data():
     cases_list = Case.query.all()
     result = cases_schema.dump(cases_list)
     return jsonify(result)
+
+@app.route('/predict', methods=['POST'])
+def predict(): 
+    air_temp = int(request.json['air_temp'])
+    humidity = int(request.json['humidity'])
+    wind_speed = int(request.json['wind_speed'])
+    active = int(request.json['active'])
+    hours = int(request.json['hours'])
+    glove = int(request.json['glove'])
+    gloves = [0.5, 1.3, 2.0, 2.4]
+    clo = gloves[int(glove)]
+    
+    print(air_temp)
+    
+    model1 = pickle.load(open('trained/dexterity.pkl', 'rb'))
+    model2 = pickle.load(open('trained/frostbite.pkl', 'rb'))
+    
+    dexterity = model1.predict(np.array([[air_temp, humidity, wind_speed, active, hours, clo]]))
+    frostbite = model2.predict(np.array([[air_temp, humidity, wind_speed, active, hours, clo]]))
+    
+    return jsonify(dexterity=dexterity[0], frostbite=frostbite[0])
     
 # @app.route('/url_variables/<string:name>/<int:age>')
 # def url_variables(name: str, age: int):
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,19 @@
-
+function glove(name) {
+    document.getElementById('glove_choice').innerHTML = name;
+    if(name == "Glove 1"){
+        var Image_Id = document.getElementById('glove_image');
+        Image_Id.src = "pictures/gloves/glove1.jpg"
+    } else if(name == "Glove 2"){
+        var Image_Id = document.getElementById('glove_image');
+        Image_Id.src = "pictures/gloves/glove2.jpg"
+    } else if(name == "Glove 3"){
+        var Image_Id = document.getElementById('glove_image');
+        Image_Id.src = "pictures/gloves/glove3.jpg"
+    } else if(name == "Glove 4"){
+        var Image_Id = document.getElementById('glove_image');
+        Image_Id.src = "pictures/gloves/glove4.jpg"
+    }
+}
 
 function tempVal(val) {
     document.getElementById("temp").innerHTML = val + " Celcius";
